82c4c45ee60d5db318bcd1f30bc9b9f7fcc23132
Isuru Ariyarathne <61895498+IsuruAriyarathne@users.noreply.github.com>
2021-07-23 11:06:56 +0530

    Add layout

---
+++ src/shared/consts.js
@@ -0,0 +1,10 @@
+// General Const
+export const SEHost = 'http://localhost:9000';
+
+// Auth Const
+export const authRedirectPath = '/';
+export const authRequestTimeoutSec = 3600;
+
+// SellerOne Api Routes
+export const loginRoute = '/';
+export const usersAdminRoute = '/v1/admin/users'
